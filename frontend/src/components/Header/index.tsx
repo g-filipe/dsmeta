595c12023e2df01833e19327d4f0f823c8e02408
g-filipe <galvao.s.filipe@gmail.com>
2022-07-16 16:56:56 -0300

    SEO improvement

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.linkedin.com/in/galvaofs/">@galvaofs</a>
+          <a href="https://www.linkedin.com/in/galvaofs/" rel = "noreferrer noopener" target = "_blank" >@galvaofs</a>
         </p>
       </div>
     </header>
